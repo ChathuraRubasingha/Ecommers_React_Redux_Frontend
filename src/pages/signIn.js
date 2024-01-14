232764294ef090c6b5878b259973f19964ef56d7
ChathuraRubasingha <79496529+ChathuraRubasingha@users.noreply.github.com>
2024-01-14 22:45:13 +0530

    login

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,22 +1,38 @@
 import React, { useState } from "react";
 import "./signin.css";
+import axios from "axios";
 
 function SignIn() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const isLogged = true;
   const handleSignIn = () => {
-    if (isLogged) {
-      window.location.href = "/home";
-    }
+    const data = {
+      email: username,
+      password: password,
+    };
+    const Login = async () => {
+      try {
+        const res = await axios.post("http://localhost:8080/login", data);
+        if (res.status === 200) {
+          alert("Login Success");
+          window.location.href = "/home";
+        }
+      } catch (err) {
+        alert(err.response.data.errorMessage);
+      }
+    };
+    Login();
+    // if (isLogged) {
+    //   window.location.href = "/home";
+    // }
   };
   return (
     <div className="signin-container">
       <h2>Sign In</h2>
       <br></br>
       <form className="signin-form">
-        <label>Username:</label>
+        <label>Email:</label>
         <input
           type="text"
           value={username}
@@ -33,7 +49,7 @@ function SignIn() {
         <button type="button" onClick={handleSignIn} className="signin-button">
           Sign In
         </button>
-        <br/>
+        <br />
         <div className="signup">
           <p> If you don't have an Account</p>
           <a href="/signup">Sign up</a>
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./signup.css";
+import axios from "axios";
 
 function SignUp() {
   const [username, setUsername] = useState("");
@@ -7,41 +8,50 @@ function SignUp() {
   const [password, setPassword] = useState("");
 
   const handleSignUp = () => {
-    // Perform signup logic here
-    console.log(
-      `Signing up with username: ${username}, email: ${email}, and password: ${password}`
-    );
+    const body = {
+      username: username,
+      email: email,
+      password: password,
+    };
+    const sendFormData = async () => {
+      try {
+        const res = await axios.post("http://localhost:8080/create-user", body);
+        console.log(res);
+        if (res.status === 200) {
+          alert("User Created successful!");
+        }
+      } catch (err) {
+        console.log(err);
+        alert(err.response.data)
+      }
+    };
+    sendFormData();
   };
   return (
     <div className="signup-container">
       <h2>Sign Up</h2>
+      <br />
       <form className="signup-form">
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <br />
         <button type="button" onClick={handleSignUp} className="signup-button">
           Sign Up
